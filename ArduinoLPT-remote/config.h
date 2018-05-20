05de50d4b68d446c97a56e12b3e304935287a7ff
François Lorrain <francois.lorrain@gmail.com>
2018-05-20 18:56:02 +0200

    Initial git import
    Working remote for the ArduinoLPT project based on NanoPro + NRF24L01P

---
+++ ArduinoLPT-remote/config.h
@@ -0,0 +1,101 @@
+/*
+ * 	El Cheapo Remote control unit
+ *
+  * 	Copyright (c) 2013 Francois Lorrain (francois.lorrain@gmail.com)
+ *
+ * This source file is subject of the GNU general public license 2 or later,
+ * available on the web at : http://www.gnu.org/licenses/gpl.txt
+ *
+ * This is meant to be a fun project to construct you own DCC command station
+ * with an Arduino board and some standard boards.
+ * The code is freely available under the GPL so it can be easily customized
+ * to your own needs.
+ * Hopefully it contains some great ideas, and you are welcome to add your own ...
+ *
+ *
+ */
+
+/*
+ * File : config.h
+ * Purpose : All global definitions and hardware related defines
+ *
+*/
+//-----------------------------------------------------------------
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+
+//========================================================================
+// 1. Processor Definitions
+//========================================================================
+//
+// Nano Pro remote uses a ATMega328P on an arduino nano pro
+
+
+#undef DEBUG
+
+//========================================================================
+// 2. Port Definitions
+//========================================================================
+// Port A
+
+// Port B
+
+#define PortB0			0		// out
+#define PB1_OC1A 		1		// out
+#define PB_NRF_CSN		2		// out
+#define MOSI			3		// out
+#define MISO			4		// in
+#define SCK				5		// out
+#define PortB6			6		// out
+#define PortB7			7		// out
+
+#define PORTB_DIRECTION	((uint8_t)0xef)   	// Output PINs for PORTB
+
+
+// Port C
+#define ADC0		0
+//#define ADC1 		1
+#define PC1_POT		1		// out
+#define ADC2		2
+//#define ADC3		3
+#define PC3_POT		3		// out
+#define SDA			4
+#define SCL			5
+#define ADC6		6
+#define	ADC7		7
+
+
+#define PORTC_DIRECTION	((uint8_t)0x0A)   	// Output PINs for PORTC
+
+// Port D
+#define PortD0			0		// in (RX)
+#define PortD1 			1		// out (TX)
+#define PD_NRF_IRQ		2		// in (INT0)
+#define PD_NRF_CE		3		// out
+#define PD_PortD4		4		// in
+#define PD_PortD5		5		// in
+#define PD_PortD6		6		// in
+#define PD_PortD7		7		// in
+
+#define PORTD_DIRECTION	((uint8_t)0x0a)   	// Output PINs for PORTD
+
+//========================================================================
+// 3. System Definitions
+//========================================================================
+
+#ifndef FALSE
+#define FALSE       0
+#endif
+#ifndef TRUE
+#define TRUE        (!FALSE)
+#endif
+
+#define OK TRUE
+#define NOK FALSE
+
+
+typedef enum {dcc_off, analog, digital} tmode;
+typedef enum {off, forward, backward} tdirection;
+
+#endif   // config.h
---
+++ ArduinoLPT-remote/main.cpp
@@ -0,0 +1,131 @@
+/*
+ * main.c
+ *
+ *  Created on: May 1st 2014
+ *      Author: francois
+ *      Nano Pro Remote project
+ */
+
+
+#include "elcheapo_remote.h"
+#include "adc.h"
+#include "timer0.h"
+#include "HardwareSerial.h"
+
+#include "hal_nrf_reg.h"
+#include "hal_nrf.h"
+
+#include "radio.h"
+#include "LowPower.h"
+
+int16_t adc_value;
+uint8_t radio_data[RF_PAYLOAD_LENGTH];
+
+int main(void) {
+	uint8_t status;
+	uint8_t count;
+	uint8_t ID = 0;
+
+	// see config.h
+	DDRB=PORTB_DIRECTION;
+	DDRC=PORTC_DIRECTION;
+	DDRD=PORTD_DIRECTION;
+	//set output I/O to 1, Input to no pull-up
+	PORTB = PORTB_DIRECTION;
+	// Pull ups on input pin on PORTD
+	PORTD = 0xf0;
+	// Control Port C Digital disabled
+	DIDR0 = 0x05; // disable digital Input pin on Channel 0 / 2
+	PORTC = 0x00; // Output as Zero on C1 and C3
+
+	// set CE low (transmit packet when CE pulsed high)
+	CE_LOW();
+	Serial.begin(115200);
+	adc_init();
+	init_timer0_tick();
+	hal_spi_init(8000000);						// Init SPI at 8 MHz
+
+
+	Serial.println(F("ArduinoLPT Nano Remote "));
+	// enable interrupts
+	__builtin_avr_sei ();
+	radio_pl_init_ptx (NRF_address);
+
+	while (1) {
+		Serial.write('L');
+//		set_loop_time(200/4);
+		radio_data[0] = 2; // See radioprotocol.txt
+		radio_data[1] = ID;
+		radio_data[2] = PIND; // Read D port ...
+
+		PORTC |= (1<<PC1_POT); // Enable High level on pot
+		wait_tempo(2);
+		adc_value = get_adc(0);
+		PORTC &= ~(1<<PC1_POT);
+		radio_data[3]=adc_value >> 8;
+		radio_data[4]=adc_value & 0xff;
+
+		PORTC |= (1<<PC3_POT); // Enable High level on pot
+		wait_tempo(2);
+		adc_value = get_adc(2);
+		PORTC &= ~(1<<PC3_POT);
+		radio_data[5]=adc_value >> 8;
+		radio_data[6]=adc_value & 0xff;
+
+		hal_nrf_get_clear_irq_flags();
+
+		for (uint8_t i=0; i<7; i++) {
+			Serial.write(' ');
+			Serial.print(radio_data[i],16);
+		}
+		Serial.println();
+
+		radio_send_packet(radio_data, 7);
+		Serial.write('<');
+		set_timeout(70);
+
+		while (!radio_activity()) {
+			if (check_timeout()) {
+				Serial.write('T');
+				break;
+			}
+		};
+		status = hal_nrf_get_status();
+		Serial.print(F("St:"));
+		Serial.println(status,16);
+
+		switch (status & 0x70) {
+		case (1<<HAL_NRF_TX_DS):
+			/* Tx packet sent, ack received but no ack packet payload ... */
+			hal_nrf_get_clear_irq_flags();
+			Serial.write('>');
+			break;
+		case ((1<<HAL_NRF_TX_DS)|(1<<HAL_NRF_RX_DR)):
+			/* Tx done, Ack packet received */
+			// get it
+			count = hal_nrf_read_reg(R_RX_PL_WID);
+			hal_nrf_read_multibyte_reg(R_RX_PAYLOAD, radio_data, count);
+			// clear IRQ source
+			hal_nrf_get_clear_irq_flags();
+			if ( (count == 2) && (radio_data[0] == 3) && (ID == 0) ){
+				ID = radio_data[1];
+			}
+			Serial.write('I');
+			break;
+		case (1<<HAL_NRF_MAX_RT):
+			hal_nrf_get_clear_irq_flags();
+			/* Max Retry reached */
+			hal_nrf_flush_tx(); 						// flush tx fifo, avoid fifo jam
+			break;
+		default:
+			hal_nrf_get_clear_irq_flags();
+			break;
+		}
+		/* don't resend a packet too fast */
+//		while (!check_loop_time());
+
+		LowPower.idle(SLEEP_250MS, ADC_ON, TIMER2_OFF, TIMER1_OFF, TIMER0_ON, SPI_ON, USART0_ON, TWI_OFF);
+
+	}
+}
+
